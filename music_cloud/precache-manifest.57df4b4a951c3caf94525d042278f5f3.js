ad98851838270e553272a5b3dc848addc54efce9
z19023025 <Jeremy_Deng@wistron.com>
2021-01-21 17:49:12 +0800

    update

---
+++ music_cloud/precache-manifest.57df4b4a951c3caf94525d042278f5f3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c407ed7046029b5900b",
+    "url": "/music_cloud/css/app.06be1090.css"
+  },
+  {
+    "revision": "564ac2d7cfa59f12d325",
+    "url": "/music_cloud/css/chunk-vendors.1002c10e.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/music_cloud/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/music_cloud/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "79bc6fa0ec9ac44188b489da8c90c1ff",
+    "url": "/music_cloud/img/demo/demo01.jpg"
+  },
+  {
+    "revision": "b02071b07b87418851b2e653b3940afa",
+    "url": "/music_cloud/index.html"
+  },
+  {
+    "revision": "fdf1d4b07fb78e1ab0f3",
+    "url": "/music_cloud/js/about.4d619bfa.js"
+  },
+  {
+    "revision": "9c407ed7046029b5900b",
+    "url": "/music_cloud/js/app.7eb1153e.js"
+  },
+  {
+    "revision": "564ac2d7cfa59f12d325",
+    "url": "/music_cloud/js/chunk-vendors.a3420da8.js"
+  },
+  {
+    "revision": "b35d1b63dddcc92467f7575f425e3c1b",
+    "url": "/music_cloud/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/music_cloud/robots.txt"
+  }
+]);
